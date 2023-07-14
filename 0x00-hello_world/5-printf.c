be76fb7cab2166f1603155691f664783c2d366cd
Hani <aminyout1@gmail.com>
2023-07-13 17:49:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - A programm that 
- * Return: 0 Success
+ * main - A programm that
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
