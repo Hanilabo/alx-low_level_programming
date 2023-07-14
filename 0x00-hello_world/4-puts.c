7c6bf1a97dc505bb70022a706c49f254a51b566d
Hani <aminyout1@gmail.com>
2023-07-13 17:25:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - A program that prints
+ * Return : 0 (success)
+ */
+
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
