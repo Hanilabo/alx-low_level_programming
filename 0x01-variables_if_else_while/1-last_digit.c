94114826eee8c6e5f263d7cbab70d879db1b418a
Hani <aminyout1@gmail.com>
2023-07-16 04:47:34 -0700

    lastdegit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - A program that check last degit
+ *
+ * return (0)(Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n % 10 > 5)
+		printf("%d Last digit of\n", n, "%d is\n",n % 10 ,"%d and is greater than5);
+	return (0);
+}
