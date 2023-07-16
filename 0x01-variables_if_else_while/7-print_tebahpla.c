caf4c6a7bc0f1e552f32ec2b7d7a6fa6d0deb93f
Hani <aminyout1@gmail.com>
2023-07-16 06:50:08 -0700

    reversealphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - alphabets in revers
+ *
+ * Result: 0 (success)
+ */
+int main(void)
+{
+	char lowercase = 'z';
+
+	while (lowercase > 'a')
+	{
+		putchar(lowercase);
+			lowercase--;
+	}
+	putchar('\n');
+	return (0);
+}
