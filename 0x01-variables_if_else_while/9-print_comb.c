885c7219d7c41829ae14fe81ab6e3adc68245284
Hani <aminyout1@gmail.com>
2023-07-16 10:07:45 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,21 +3,22 @@
 /**
  * main - prints all possible combinations of single-digit numbers
  *
- * return: 0 (success)
+ *Return: 0 (success)
  */
 int main(void)
 {
 	int num = 0;
+	
 	while (num < 10)
 	{
 		putchar('0' + num);
-		num++;   
+		num++;
 		if (num < 10)
 		{
 		putchar(44);
 		putchar(32);
 		}
-	}       
+	}
 	putchar('\n');
 	return (0);
 }
