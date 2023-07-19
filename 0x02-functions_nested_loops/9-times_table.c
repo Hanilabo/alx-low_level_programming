8045799d12aeac56ce25c21f8e5a3b5c721bb4fd
Hani <aminyout1@gmail.com>
2023-07-19 10:57:44 -0700

    10times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,30 @@
  */
 void times_table(void)
 {
-	int row, col, result;
-	
-	for (row = 0; row <= 9; row++)
-	{
-		for (col = 0; col <= 9; col++)
-		{
-			result = row * col;
-			_putchar('0' + result / 10);
-			_putchar('0' + result % 10);
-			if (col < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			} else {
-				_putchar('\n');
-			}
-		}
-	}
+    int row, col, result;
+
+    for (row = 0; row <= 9; row++)
+    {
+        for (col = 0; col <= 9; col++)
+        {
+            result = row * col;
+            if (result < 10)
+            {
+                _putchar('0');
+                _putchar('0' + result);
+            }
+            else
+            {
+                _putchar('0' + result / 10);
+                _putchar('0' + result % 10);
+            }
+
+            if (col < 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
 }
