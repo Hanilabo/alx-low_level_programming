9888cfe22bae1c39a60955a263bd73e824227e17
Hani <aminyout1@gmail.com>
2023-07-19 09:46:16 -0700

    10times

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,15 +3,17 @@
 
 /**
  * _abs- absolute value
+ * @num: is number
+ * Return: num if great. -num if less
  */
 int _abs(int num)
 {
 	if (num < 0)
 	{
-		return -num;
+		return (-num);
 	}
 	else
 	{
-		return num;
+		return (num);
 	}
 }
