d5b3cd24e48aedfb51903aac4b01377c9869285e
Hani <aminyout1@gmail.com>
2023-07-19 08:17:21 -0700

    10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,12 @@
 #include"main.h"
 
 /**
- * void print_alphabet_x10(void)- 10 times alphabets
+ * print_alphabet_x10- 10 times alphabets
  */
 void print_alphabet_x10(void)
 {
-	int r = 9;
-	
+	int r = 0;
+
 	while (r < 9)
 	{
 		char lowercase = 'a';
