0fdc5559a43abfca4ab47bb03ac8ac666cf8a768
Hani <aminyout1@gmail.com>
2023-07-19 09:51:49 -0700

    10times

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<"main.h"
+
+/**
+ * print_last_digit- print last digit
+ * @num: number
+ * @lastdigit: lastdigit
+ * Return: lastdigit number
+ */
+int print_last_digit(int num)
+{
+	int lastdigit,
+	    
+	lastdigit = num % 10;
+	return (lastdigit);
+}
