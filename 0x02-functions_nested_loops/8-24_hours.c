507eeceb07e1ddf33fd8c61cc6472367282d2845
Hani <aminyout1@gmail.com>
2023-07-19 10:32:17 -0700

    10times

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,20 @@
  */
 void jack_bauer(void)
 {
-	int hf = 0;
-	int hs = 0;
-	int mf = 0;
-	int ms = 0;
-	while(mf < 5 && ms <9)
-	{
-		_putchar('0'+hf);
-		_putchar('0'+hs);
-		_putchar(':');
-		_putchar('0'+mf);
-		_putchar('0'+ms);
+	    int hf, hs, mf, ms;
 
-	}
+    for (hf = 0; hf < 24; hf++) {
+        for (hs = 0; hs < 10; hs++) {
+            for (mf = 0; mf < 6; mf++) {
+                for (ms = 0; ms < 10; ms++) {
+                    _putchar('0' + hf / 10); // Print the tens digit of hour
+                    _putchar('0' + hf % 10); // Print the ones digit of hour
+                    _putchar(':');
+                    _putchar('0' + mf); // Print the tens digit of minute
+                    _putchar('0' + ms); // Print the ones digit of minute
+                    _putchar('\n'); // Move to the next line
+                }
+            }
+        }
+    }
 }
