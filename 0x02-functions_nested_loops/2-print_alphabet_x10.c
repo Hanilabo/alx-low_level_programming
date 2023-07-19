1074c2dd3b76f6e7f12fb212bcece379227e40f4
Hani <aminyout1@gmail.com>
2023-07-19 08:08:27 -0700

    10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include<stdio>
+#include"main.h"
+
+/**
+ * void print_alphabet_x10(void)
+ */
+void print_alphabet_x10(void)
+{
+	int r=9;
+
+	while(r<9)
+	{
+		char lowercase = 'a';
+
+		while (lowercase <= 'z')
+
+		{
+			_putchar(lowercase);
+			lowercase++;
+		}
+			_putchar('\n');
+			r++;
+	}
