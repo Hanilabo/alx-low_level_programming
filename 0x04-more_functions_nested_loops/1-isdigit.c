b0aa5708c965103c480a09e2273eeb27df7e17ee
Hani <aminyout1@gmail.com>
2023-07-21 10:56:14 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+#include<ctype.h>
+
+/**
+ * _isdigit- check if digit
+ * @c: number is ASCII
+ * Retun: 1 if digit. 0 if not
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
