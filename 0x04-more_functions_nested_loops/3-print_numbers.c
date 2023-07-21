df7fa2ac617bd0500d2f821c3e36e58531c7fd6d
Hani <aminyout1@gmail.com>
2023-07-21 11:30:33 -0700

    com

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: 0 sucess
+ */
+void print_numbers(void);
+{
+	int num;
+	
+	while(num = 0, num > 9 , num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+	return (0);
+}
