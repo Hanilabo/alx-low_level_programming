3919bbe6f14cb3375138921b2a193061f2d5af96
Hani <aminyout1@gmail.com>
2023-07-21 10:50:02 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+#include<ctype.h>
+
+/**
+ * _isupper- check if uppercase
+ *
+ * Return: 1 if upper. 0 if not
+ */
+int _isupper(int c)
+{
+	if (_isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
