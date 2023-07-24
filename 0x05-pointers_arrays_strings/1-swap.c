e65b6b50891cd69858cc1afe82e02c5df278ff29
Hani <aminyout1@gmail.com>
2023-07-24 08:04:13 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include"main.h"
+
+/**
+ * swap_int- swab numbers
+ *
+ * return: 0 (success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
